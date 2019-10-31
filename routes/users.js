ccfcd499320efd0b67962777db75655ad6b7151e
Albert Li <myerp@126.com>
2019-10-31 18:08:04 +0800

    add users.cool

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('res.send: respond with a resource');
 });
 
+router.get('/cool', function (req, res, next) {
+  res.send('cool:res.send: You are cool!');
+});
+
+
 module.exports = router;
